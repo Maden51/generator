99551592459fea37e5168af62d0132983b0f5e11
Maden5126 <madenon123@gmail.com>
2021-10-20 15:05:09 +0300

    1.0.0

---
+++ src/js/team.js
@@ -0,0 +1,31 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    this.members.forEach((item) => {
+      if (JSON.stringify(item) === JSON.stringify(character)) {
+        throw new Error('Такой персонаж уже есть в команде');
+      }
+    });
+    this.members.add(character);
+  }
+
+  addAll(...arr) {
+    arr.forEach((item) => {
+      this.members.add(item);
+    });
+    return this;
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+
+  * [Symbol.iterator]() {
+    for (const person of this.members) {
+      yield person;
+    }
+  }
+}
